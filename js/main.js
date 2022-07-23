7be6c62cbe16c25d164ca7aac69ea31072eae9e9
Navruzbek2103 <navrozbek2306@gmail.com>
2022-07-24 00:31:25 +0500

    Added new element

--- js/main.js
+++ js/main.js
@@ -9,20 +9,34 @@ elForm.addEventListener("input", function(mission){
   mission.preventDefault();
 
   var count = 0;
-  var compute = 0;
+  var box = [];
 
   if(elInput.value.length != 0){
     if(!(isNaN(elInput.value))){
       for(i = 1; i <= elInput.value; i++){
+        var compute = 0;
         if(elInput.value % i == 0){
           count++;
         }
+        for(j = 1; j <= i; j++){
+          if(i % j == 0){
+            compute++;
+          }
+        }
+        if(compute == 2){
+          for(p = 2; p <= compute; p++){
+            box.push(i);
+            elParagraphTwo.textContent = elInput.value +  " gacha bo'lgan tub sonlar: " + box;
+
+          }
+
+        }
       }
       if(count == 2){
-        elParagraphOne.textContent = elInput.value + " - Tub son"
+        elParagraphOne.textContent = elInput.value + " -> tub son"
       }
       else{
-        elParagraphOne.textContent = elInput.value + " - Tub son emas"
+        elParagraphOne.textContent = elInput.value + " -> tub son emas"
       }
     }
     else{
@@ -30,6 +44,8 @@ elForm.addEventListener("input", function(mission){
     }
   }
   else{
+    elParagraphOne.textContent = "";
+    elParagraphTwo.textContent = "";
     alert("Ma'lumot kiritilmadi, yoki bo'sh")
   }
 })
